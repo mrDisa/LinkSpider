5fc6110199fba8ca3278e1b806467283d11385a3
Daniil Isaverdov <daniil.isaverdov@gmail.com>
2026-08-11 16:45:46 +0300

    Implement CheckLink

--- internal/checker/checker.go
+++ internal/checker/checker.go
@@ -1,1 +1,33 @@
-package checker 
+package checker
+
+import (
+	"net/http"
+)
+
+type LinkResult struct {
+	URL        string
+	StatusCode int
+	Alive      bool
+	Error 	   string
+}
+
+func CheckLink(url string) LinkResult {
+	resp, err := http.Head(url)
+	if err != nil {
+		return LinkResult {
+			URL: url,
+			StatusCode: 0,
+			Alive: false,
+			Error: err.Error(),
+		}
+	}
+	defer resp.Body.Close()
+
+	aliveLink := resp.StatusCode >= 200 && resp.StatusCode < 400
+	return LinkResult {
+		URL: url,
+		StatusCode: resp.StatusCode,
+		Alive: aliveLink,
+		Error: "",
+	}
+}
