810388377d2ca2b8f9c9511bc504bfa9ec57b0ca
Daniil Isaverdov <daniil.isaverdov@gmail.com>
2026-08-11 17:41:20 +0300

    Wire up fetcher, parser, checker into working pipeline

--- main.go
+++ main.go
@@ -1,16 +1,56 @@
 package main
 
 import (
+    "flag"
     "fmt"
-    "io"
+    "net/url"
+    "linkspider/internal/checker"
     "linkspider/internal/fetcher"
+    "linkspider/internal/parser"
 )
 
 func main() {
-	reader, err := fetcher.FetchHTML("https://example.com")
+	urlPtr := flag.String("url", "", "URL to check")
+	flag.Parse()
+	pageURL := *urlPtr
+
+	if pageURL == "" {
+		fmt.Println("Ошибка: нужно указать -url")
+		return
+	}
+
+	reader, err := fetcher.FetchHTML(pageURL)
 	if err != nil {
 		fmt.Println("error: ", err)
+		return
 	}
-	data, _ := io.ReadAll(reader)
-	fmt.Println(string(data))
+	links, err := parser.ExtractLinks(reader)
+	if err != nil {
+		fmt.Println("error: ", err)
+		return
+	}
+
+	base, err := url.Parse(pageURL)
+	if err != nil {
+		fmt.Println("error: ", err)
+		return
+	}
+
+	for _, rawLink := range links {
+		ref, err := url.Parse(rawLink)
+		if err != nil {
+			continue
+		}
+		resolved := base.ResolveReference(ref)
+		resolvedURL := resolved.String()
+		link := checker.CheckLink(resolvedURL)
+		
+		if link.Error != "" {
+			fmt.Printf("Link - %s - STATUS: %d [FAIL]: %s\n", link.URL, link.StatusCode, link.Error)
+		} else if link.Alive {
+			fmt.Printf("Link - %s - STATUS: %d [OK]\n", link.URL, link.StatusCode)
+		} else {
+			fmt.Printf("Link - %s - STATUS: %d [BROKEN]\n", link.URL, link.StatusCode)
+		}
+	}
 }
